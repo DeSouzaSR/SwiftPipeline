b840cb9a6a62ae4787bab12a315c86201c7e4154
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2017-04-03 17:05:52 -0300

    Modify run.py. It's take arguments and create directory with same name

--- run.py
+++ run.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 import yaml
-import os, sys 
-
-
+import os, sys
 
 def create_file():
     with open(sys.argv[1], 'r') as f:
         config_file = yaml.load(f)
     
-    simulation_name = config_file['simulation_name']
-    
+    simulation_name = sys.argv[1]
+    simulation_name = simulation_name.split('.')[0]
     os.mkdir(simulation_name)
     
-    
-    
+    os.mkdir()
 if __name__ == '__main__':
     create_file()
     
