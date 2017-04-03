8ee69c3d4be59edeee10e54305306a20fe28d034
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2017-04-02 21:38:36 -0300

    Adding os and sys modules

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 import yaml
-import system 
+import os, sys 
+
+
 
 def create_file():
-    with open('config.yaml', 'r') as f:
+    with open(sys.argv[1], 'r') as f:
         config_file = yaml.load(f)
     
     simulation_name = config_file['simulation_name']
     
-    system.mkdir(simulation_name)
+    os.mkdir(simulation_name)
     
     
     
