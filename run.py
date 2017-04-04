ba97c8dd4d67be65bd3e83e2fb98a17d53c39780
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2017-04-04 18:40:18 -0300

    Adding loop to create directories

--- run.py
+++ run.py
@@ -3,15 +3,20 @@
 import yaml
 import os, sys
 
-def create_file():
-    with open(sys.argv[1], 'r') as f:
-        config_file = yaml.load(f)
-    
-    simulation_name = sys.argv[1]
+def create_file(file):    
+    simulation_name = file
     simulation_name = simulation_name.split('.')[0]
     os.mkdir(simulation_name)
     
-    os.mkdir()
+    with open(file, 'r') as f:
+        config_file = yaml.load(f)
+
+    suffix_simu = config_file['suffix_simu']
+    clones_numbers = config_file['clones_numbers']
+    
+    for i in range(len(suffix_simu)):
+        os.mkdirs(str(simulation_name) + str(simulation_name) + str(i))
+        
 if __name__ == '__main__':
-    create_file()
+    create_file(sys.argv[1])
     
