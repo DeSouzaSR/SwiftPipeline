915a9a31384564df73fe3880a18e6130921843cc
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2017-04-06 10:56:45 -0300

    many changes

--- run.py
+++ run.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-import yaml
+from configobj import ConfigObj
 import os, sys
+import shutil
 
 def create_file(file):    
     simulation_name = file
     simulation_name = simulation_name.split('.')[0]
-    os.mkdir(simulation_name)
+
+    if os.path.isdir(simulation_name):
+        shutil.rmtree(simulation_name)
+        os.mkdir(simulation_name)
+    else:
+        os.mkdir(simulation_name)
     
     with open(file, 'r') as f:
         config_file = yaml.load(f)
 
     suffix_simu = config_file['suffix_simu']
-    clones_numbers = config_file['clones_numbers']
-    
-    for i in range(len(suffix_simu)):
-        os.mkdirs(str(simulation_name) + str(simulation_name) + str(i))
+#    clones_numbers = config_file['clones_numbers']
+
+
+    for i in range(1,len(suffix_simu) + 1):
+        os.makedirs(str(simulation_name)+ "/" + str(simulation_name) + "{:03d}".format(i))
         
 if __name__ == '__main__':
     create_file(sys.argv[1])
